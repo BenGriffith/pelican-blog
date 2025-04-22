4b2fa4a611b514b4f3f8ee0056493a6acf037e2b
Ben Griffith <bengriffith@outlook.com>
2025-04-21 20:33:39 -0500

    Update documentation

--- publishconf.py
+++
@@ -1,9 +0,0 @@
-from pelicanconf import *
-
-SITEURL = 'https://bengriffith.github.io'
-RELATIVE_URLS = False
-
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
-
-DELETE_OUTPUT_DIRECTORY = True
